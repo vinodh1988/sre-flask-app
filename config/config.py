9e8ac49bddbdfcc86bdc6bf535aaede16d6762a9
vinodh1988 <k.c.vinodh@hotmail.com>
2022-04-19 15:59:36 +0530

    first-commit

--- config/config.py
+++ config/config.py
@@ -1,3 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']="mysql+pymysql://root:password@localhost/sre"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+db=SQLAlchemy(app)
