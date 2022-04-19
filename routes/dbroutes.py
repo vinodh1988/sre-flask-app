06522ddeadbfcdd20dab64c83179596c080bff8f
vinodh1988 <k.c.vinodh@hotmail.com>
2022-04-19 17:00:15 +0530

    crud

--- routes/dbroutes.py
+++ routes/dbroutes.py
@@ -1,5 +1,5 @@
 from config import app,db
-from flask import jsonify,request
+from flask import jsonify,request,abort
 from models import people,DbPerson
 
 @app.route("/dbpeople")
@@ -7,4 +7,18 @@ def getDbPeople():
     listp=DbPerson.query.all()
     result = [x.serialize() for x in listp]
     return jsonify(result)
+
+
+@app.route("/dbpeople",methods=['POST'])
+def processDepartments():
+    try:
+        input=request.get_json()
+        sno=input['sno']
+        name=input['name']
+        city=input['city']
+        db.session.add(DbPerson(sno,name,city))
+        db.session.commit()
+        return {"status": "success"}, 201
+    except:
+        abort({'status':"Internal server error"},500)
 
